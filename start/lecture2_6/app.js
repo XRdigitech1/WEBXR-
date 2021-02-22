833f7f0b7e7ca804dbcfc78bc3cfe4f6fbd4a444
XRdigitech <Nikhil@LAPTOP-VJLH3IPR>
2021-02-22 18:21:23 +0530

    DuckAdded

--- start/lecture2_6/app.js
+++ start/lecture2_6/app.js
@@ -33,7 +33,8 @@ class App{
 		container.appendChild( this.renderer.domElement );
 		
         //Add code here
-        
+        this.LoadingBar = new LoadingBar();
+        this.loadGLTF();
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.target.set(0, 3.5, 0);
@@ -62,6 +63,24 @@ class App{
     
     loadGLTF(){
         const self = this;
+        const loader = new GLTFLoader().setPath('../../assets/');
+        loader.load('Duck.glb' , function(gltf) 
+        {self.chair = gltf.scene;
+            self.scene.add(gltf.scene);
+            const bbox = new THREE.Box3().setFromObject(gltf.scene);
+            console.log('min:${ vector3ToString(bbox.min,2)} - max:${vector3ToString(bbox.max, 2)}' );
+            self.LoadingBar.visible = false;
+            self.renderer.setAnimationLoop(self.render.bind(self));
+         },
+         function(xhr)
+         {
+             self.LoadingBar.progress = xhr.loaded/xhr.total;
+         },
+         function(err)
+         {
+             console.log('an error happened');
+         }
+         )
     }
     
     loadFBX(){
