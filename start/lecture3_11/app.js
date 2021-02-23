b021f42d72b23f719539e0f19504a5cd4dc0658c
XRdigitech <Nikhil@LAPTOP-VJLH3IPR>
2021-02-23 14:42:45 +0530

    Update app.js

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -142,6 +142,29 @@ class App{
         const btn = new ARButton( this.renderer, { onSessionStart, onSessionEnd } );
         
         //Add gestures here
+        this.gestures = new ControllerGestures(this.renderer);
+
+        this.gestures.addEventListener('tap', (ev) => {
+            console.log(tap);
+            self.ui.updateElement('info' , 'tap');
+
+            if(!self.knight.object.visible)
+            {
+                self.knight.object.visible = true;
+                self.knight.object.position.set(0, -0.3, -0.5).add(ev.position);
+                self.scene.add(self.knight.object);
+            }
+        });
+
+        this.gestures.addEventListener('swipe' , (ev)=> {
+            console.log(ev),
+            self.ui.updateElement('info', 'swipe ${ev.direction}');
+            if(self.knight.object.visible){
+                self.knight.object.visible = false;
+                self.knight.remove(self.knight.object);
+
+            }
+        })
         
         this.renderer.setAnimationLoop( this.render.bind(this) );
     }
