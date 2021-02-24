576f237ab636aff3ca640baf3b366e2a84bd4177
XRdigitech <Nikhil@LAPTOP-VJLH3IPR>
2021-02-24 15:31:43 +0530

    Update app.js

--- start/lecture3_10/app.js
+++ start/lecture3_10/app.js
@@ -142,8 +142,10 @@ class App{
         }
         
         const btn = new ARButton(this.renderer, {onSessionStart , onSessionEnd , sessionInit : {optionalFeatures: ['dom-overlay'], domOverlay : {root: document.body}}});
+        
         const controller = this.renderer.xr.getController(0);
         controller.addEventListener('connected', onConnected);
+        
         this.scene.add(controller);
         this.controller = controller;
 
